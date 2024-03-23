5e5745e2d97b547a3c22e91df6bf75ac2f91ec50
Ezra Mallo <ezra.c.mallo@gmail.com>
2024-03-22 22:29:14 -0600

    Add project files.

---
+++ Courses.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Models;
+
+public class Courses
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public int? Amount { get; set; }
+    public DateTime? CreatedDate { get; set; }
+}
+
---
+++ Data/Migrations/20240319200922_[CoursesMigration].cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class CoursesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
